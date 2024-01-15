8e01cad1d80446e3c8aa9531d729c83a02198b25
HeroRicky_GAMES <ricojn9@gmail.com>
2024-01-15 14:59:08 -0300

    Primeira aula em C#

---
+++ PrimeiroProjetoEmCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//Programado por HeroRickyGames
+
+namespace PrimeiroProjetoEmCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+        }
+    }
+}
